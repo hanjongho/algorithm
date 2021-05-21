44ac5afd5e53dfaa6f2337dd212e70c1937ac055
hanjongho <lgwl81@gmail.com>
2021-05-21 09:40:00 +0900

    구간 합 구하기

--- segmenttree/2042.cpp
+++ segmenttree/2042.cpp
@@ -1,65 +1,67 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int N, M, K;
-int arr[1000001];
-long long tree[4000004];
+long long arr[1000001], tree[4000004];
 
-long long init(int start, int end, int node){
-    if(start == end)
-        return tree[node] = arr[start];
-    
-    int mid = (start + end) / 2;
-    
-    return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
+long long init(int idx, int start, int end)
+{
+    if (start == end)
+        return tree[idx] = arr[start];
+
+    return tree[idx] = init(idx * 2, start, (start + end) / 2) + init(idx * 2 + 1, (start + end) / 2 + 1, end);
 }
 
-long long sum(int start, int end, int node, int left, int right){
-    if(left > end || right < start)
-        return 0;
+long long sum(int idx, int start, int end, int left, int right)
+{
+    if (right < start || left > end)
+        return (0);
+
+    if (start >= left && end <= right)
+        return tree[idx];
     
-    if(left <= start && end <= right)
-        return tree[node];
-    
-    int mid = (start + end) / 2;
-    
-    return sum(start, mid, node * 2, left, right) + sum(mid+1, end, node * 2 + 1, left, right);
+    return sum(idx * 2, start, (start + end) / 2, left, right) +
+            sum(idx * 2 + 1, (start + end) / 2 + 1, end, left, right);
 }
 
-void update(int start, int end, int node, int index, int change){
-    if(index < start || index > end)
-        return;
-    
-    tree[node] += change;
-    if(start == end)
-        return;
-    
-    int mid = (start + end) / 2;
-    update(start, mid, node * 2, index, change);
-    update(mid+1, end, node * 2 + 1, index, change);
-}
+void update(int idx, int start, int end, int findIdx, long long changeValue)
+{
+    if (findIdx < start || findIdx > end)
+        return ;
 
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-   
-    cin >> N >> M >> K;
-    
-    for(int i=0; i<N; i++)
-        cin >> arr[i];
-    
-    init(0, N-1, 1);
-
-    for(int i=0; i<M+K; i++){
-        int a, b, c;
-        cin >> a >> b >> c;
-        if(a == 1){ // 1이면 변경
-            update(0, N-1, 1, b-1, c-arr[b-1]);
-            arr[b-1] = c;
-        }
-        else if(a == 2) // 2이면 출력
-            cout << sum(0, N-1, 1, b-1, c-1) << "\n";
+    if (start == end)
+    {
+        tree[idx] = changeValue;
+        return ;
     }
-    
-    return 0;
+    update(idx * 2, start, (start + end) / 2, findIdx, changeValue);
+    update(idx * 2 + 1, (start + end) / 2 + 1, end, findIdx, changeValue);
+    tree[idx] = tree[idx * 2] + tree[idx * 2 + 1];
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int N, M, K;
+    cin >> N >> M >> K;
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+
+    init(1, 0, N - 1);
+
+    for (int i = 0; i < M + K; i++)
+    {
+        int a, b;
+        long long c;
+        cin >> a >> b >> c;
+        if (a == 1)
+            update(1, 0, N - 1, b - 1, c);
+        else
+            cout << sum(1, 0, N - 1, b - 1, c - 1) << "\n";
+    }
+
+    return (0);
 }
