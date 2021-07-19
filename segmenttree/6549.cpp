2fd1b625eb3ced44cee502b5c994dbabe3af0ad2
hanjongho <lgwl81@gmail.com>
2021-07-19 22:11:16 +0900

    구간 합 구하기

--- segmenttree/6549.cpp
+++ segmenttree/6549.cpp
@@ -1,45 +1,85 @@
 #include <iostream>
-#include <stack>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int n;
+int arr[100001], tree[400004];
 
-int main(){
-    while(1){
-        cin >> n;
-        if(!n)
-            break;
-        
-        stack<int> s;
-        int h[100001];
-        long long area = 0, height, width;
-        
-        for (int i = 0; i < n; i++)
-            cin >> h[i];
- 
-        for(int i = 0; i < n; i++){
-            while(!s.empty() && h[s.top()] > h[i]){
-                height = h[s.top()];
-                width = i;
-                s.pop();
-                if(!s.empty())
-                    width = i - s.top() - 1;
-                area = max(area, width * height);
-                cout << area << "\n";
-            }
-            s.push(i);
-        }
-        
-        while(!s.empty()){
-            height = h[s.top()];
-            width = n;
-            s.pop();
-            if(!s.empty())
-                width = n - s.top() - 1;
-            area = max(area, width *height);
-        }
-        cout << area << "\n";
-    }
+int init(int idx, int start, int end)
+{
+    if (start == end)
+        return tree[idx] = start;
+
+    init(idx * 2, start, (start + end) / 2);
+    init(idx * 2 + 1, (start + end) / 2 + 1, end);
+    if (arr[tree[idx * 2]] < arr[tree[idx * 2 + 1]])
+        return tree[idx] = tree[idx * 2];
+    else
+        return tree[idx] = tree[idx * 2 + 1];
+}
+
+int minIdx(int idx, int start, int end, int left, int right)
+{
+    if (start > right || end < left)
+        return (-1);
+
+    else if (left <= start && end <= right)
+        return tree[idx];
     
-    return 0;
+    int leftQuery = minIdx(idx * 2, start, (start + end) / 2, left, right);
+    int rightQuery = minIdx(idx * 2 + 1, (start + end) / 2 + 1, end, left, right);
+    
+    if (leftQuery == -1)
+        return rightQuery;
+    if (rightQuery == -1)
+        return leftQuery;
+
+    if (arr[leftQuery] < arr[rightQuery])
+        return leftQuery;
+    else
+        return rightQuery;
+}
+
+long long getMaxArea(int N, int start, int end)
+{
+    int idx = minIdx(1, 0, N - 1, start, end);
+    long long area = (end - start + 1) * (long long)arr[idx];
+    
+    if (start < idx)
+        area = max(area, getMaxArea(N, start, idx - 1));
+
+    if (idx < end)
+        area = max(area, getMaxArea(N, idx + 1, end));
+
+    return area;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    
+    while (1)
+    {
+        int N;
+        cin >> N;
+        if (N == 0)
+            break ;
+        
+        for (int i = 0; i < 400004; i++)
+        {
+            if (i < 100001)
+                arr[i] = 0;
+            tree[i] = 0;
+        }
+
+        for (int i = 0; i < N; i++)
+            cin >> arr[i];
+        
+        init(1, 0, N - 1);
+        
+        cout << getMaxArea(N, 0, N - 1) << "\n";
+    }
+
+    return (0);
 }
